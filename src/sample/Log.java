e7884b965e66aa3ee2a3696fef433fc396ce9347
Gleb Lagutko <gleblagutko@gmail.com>
2020-04-21 11:46:41 +0300

    123

--- src/sample/Log.java
+++ src/sample/Log.java
@@ -7,20 +7,14 @@ import java.util.List;
 
 public class Log implements Observer {
     private Text text;
-    private List<String> log = new ArrayList<>();
 
-    public Log( Text text) {
+    public Log(Text text) {
 
         this.text = text;
     }
 
     @Override
     public void update(String s) {
-        log.add(s);
-        StringBuilder str = new StringBuilder();
-        for (String st : log) {
-            str.append(st).append(" ");
-        }
-        text.setText(str.toString());
+        text.setText(text.getText() + " " + s);
     }
 }
